157cea901625c757ecf4915fc884df5b1ae47a3b
skpk24 <skpk24@gmail.com>
2020-01-19 20:11:15 +0530

    Update App.java

--- solid-principles/InterfaceSegregrationPrinciple/App.java
+++ solid-principles/InterfaceSegregrationPrinciple/App.java
@@ -1,1 +1,16 @@
+public class App {
 
+	public static void main(String[] args) {
+		Manager manager = new Manager();
+		
+		manager.setEmployee(new Architect());
+		manager.manageEmployee();
+		
+		manager.setEmployee(new Developer());
+		manager.manageEmployee();
+		
+		manager.setEmployeeWorkInterface(new Robot());
+		manager.assignWork();
+	}
+
+}
