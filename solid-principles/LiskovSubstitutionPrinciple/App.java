65d0a2c413f34b16217c1c4ac8c5d56b7811a51e
skpk24 <skpk24@gmail.com>
2020-01-19 18:16:05 +0530

    Update App.java

--- solid-principles/LiskovSubstitutionPrinciple/App.java
+++ solid-principles/LiskovSubstitutionPrinciple/App.java
@@ -1,6 +1,21 @@
-public class App{
-  public static void main(String args[]){
-  
-  }
-  
+public class App {
+
+	public static void main(String[] args) {
+    
+		Rectangle rectangle = new Rectangle();
+		
+		rectangle.setLength(10);
+		rectangle.setWidth(20);
+		
+		System.out.println(rectangle.calculateArea());
+		
+		Rectangle squar = new Squar();
+		
+		squar.setLength(20);
+		
+		System.out.println(squar.calculateArea());
+
+	}
+
 }
+
