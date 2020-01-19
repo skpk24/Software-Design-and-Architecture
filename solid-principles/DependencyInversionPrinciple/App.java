c06fe5c16e46d5a1cae39a138cc370d2dd3f86a5
skpk24 <skpk24@gmail.com>
2020-01-19 20:25:04 +0530

    Update App.java

--- solid-principles/DependencyInversionPrinciple/App.java
+++ solid-principles/DependencyInversionPrinciple/App.java
@@ -1,1 +1,15 @@
 
+public class App {
+
+	public static void main(String[] args) {
+		Manager manager = new Manager();
+		
+		manager.setEmployee(new Developer());
+		manager.manage();
+		
+		manager.setEmployee(new Architect());
+		manager.manage();
+		
+	}
+
+}
