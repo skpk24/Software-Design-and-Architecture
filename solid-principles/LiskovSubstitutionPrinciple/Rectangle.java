29022e35f993c741baeb3691453b6de9ccc01ce5
skpk24 <skpk24@gmail.com>
2020-01-19 18:17:03 +0530

    Create Rectangle.java

---
+++ solid-principles/LiskovSubstitutionPrinciple/Rectangle.java
@@ -0,0 +1,25 @@
+public class Rectangle {
+	
+	protected double length;
+	protected double width;
+	
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double calculateArea() {
+		return length * width;
+	}
+}
